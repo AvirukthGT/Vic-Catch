56ef9bd551f34e60f8577dcb874e56dcd502c686
avirukthgt <avirukth@gmail.com>
2026-05-12 19:34:07 +1000

    bring the raw OSM Parquet file into the dbt staging layer and convert the latitude/longitude into BigQuery ST_GEOGRAPHY points

---
+++ dbt_vic/models/staging/stg_osm_features.sql
@@ -0,0 +1,23 @@
+with source as (
+    select * from {{ source('datalake', 'ext_osm_features') }}
+),
+
+renamed as (
+    select
+        osm_id,
+        osm_type,
+        feature_group,
+        
+        -- Keeping raw coordinates for debugging
+        cast(lat as float64) as latitude,
+        cast(lon as float64) as longitude,
+        
+        --  Create the BigQuery Geography Point
+        ST_GEOGPOINT(cast(lon as float64), cast(lat as float64)) as feature_geometry
+        
+    from source
+    -- Filter out any bad coordinates just in case
+    where lat is not null and lon is not null
+)
+
+select * from renamed
